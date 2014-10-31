3e26ed116ab849e754c069a9b9639cb057bb8554
Michal Špaček <tupinek@gmail.com>
2014-10-31 08:00:57 +0100

    Add Video::Delay module.

---
+++ Delay.pm
@@ -0,0 +1,46 @@
+package Video::Delay;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.02;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+ Video::Delay - Perl classes for delays between frames generation.
+
+=head1 SEE ALSO
+
+L<Video::Delay::Array>,
+L<Video::Delay::Const>,
+L<Video::Delay::Func>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Video-Delay>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD 2-Clause License
+
+=head1 VERSION 
+
+0.02
+
+=cut
